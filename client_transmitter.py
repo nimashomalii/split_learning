42c7a2e821f6c332250386e7e180cd89906cb7b5
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-09-13 20:00:43 +0330

    Update client_transmitter.py

--- client_transmitter.py
+++ client_transmitter.py
@@ -5,10 +5,12 @@ import time
 class Transmitter : 
     def __init__(self,server_url) : 
         self.server_url = server_url #" https://d5e33cbc658b.ngrok-free.app/is_even"
-    def send_data(self):
+    def send_data(self , x , y):
+        x = x.detach().cpu().tolist()
+        label = label.detach().cpu().tolist()
         data = {
-            'list1':[1 , 2],
-            'list2': [3 , 4]
+            'list1':x,
+            'list2': label
         }
         headers = {'Content-Type': 'application/json'}
         response = requests.post(self.server_url, data=json.dumps(data), headers=headers)
