92e9e5bad343f785d4f4e9b67b177fb2405523f8
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-09-13 19:37:22 +0330

    Update client_transmitter.py

--- client_transmitter.py
+++ client_transmitter.py
@@ -7,8 +7,9 @@ class Transmitter :
         self.server_url = server_url #" https://d5e33cbc658b.ngrok-free.app/is_even"
     def send_data(self , x , label) :  #this method will send the output of the first neural network to the server 
         x_list = x.detach().cpu().tolist()
+        label_list = label.detach().cpu.tolist()
         data  = {'value' : x_list ,  
-                 'label' : label }
+                 'label' : label_list }
         headers = {'Content-Type': 'application/json'}
         response = requests.post(self.server_url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
