46f0a2bd342ce5a2ea31ed5387d26d16b8f4c5ad
nimashomalii <106135440+nimashomalii@users.noreply.github.com>
2025-09-14 12:50:53 +0330

    Update client_transmitter.py

--- client_transmitter.py
+++ client_transmitter.py
@@ -1,20 +1,34 @@
 import requests
 import json
 import time
+import torch 
 
 class Transmitter : 
     def __init__(self,server_url) : 
         self.server_url = server_url #" https://d5e33cbc658b.ngrok-free.app/is_even"
-    def send_data(self , x , label):
+
+    def send_data(self , x , label ,device ,  status  ): #status if is train or test 
         x = x.detach().cpu().tolist()
         label = label.detach().cpu().tolist()
-        data = {
-            'list1':x,
-            'list2': label
-        }
+        if status == 'train' : 
+            data = {
+                'prediction_iput':x,
+                'label': label ,
+                'status' : status
+            }
+        elif status == 'test' : 
+            data = {
+                'prediction_iput':x,
+                'status' : status
+            }
         headers = {'Content-Type': 'application/json'}
         response = requests.post(self.server_url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
         result = response.json()
-        return result
+        if status == 'train' : # result = {'grad' :  }
+            grad = result['grad']
+            return torch.tensor(grad).to(device)
+        elif status == 'test' :
+            prediction = torch.tensor(result['prediction']).to(device)
+            return  prediction
 
